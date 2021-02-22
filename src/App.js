69488e3249db7a241be2974e2cab7514d33340ac
Sukhwinder Singh <sukhwinder.singh@zextras.com>
2021-02-22 15:46:27 +0100

    Add quantity field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { useState } from 'react'
+
+const minQuantity = 1
+
+function CoerceQuantity(value) {
+  let quantity = Number(value)
+  if (quantity >= minQuantity) return Math.floor(quantity)
+  else return minQuantity
+}
+
 export function App() {
+  const [quantity, setQuantity] = useState(minQuantity)
   const onSubmit = (event) => {
     event.preventDefault()
     console.log('on submit')
+  }
+  const onQuantityChange = (event) => {
+    setQuantity(CoerceQuantity(event.target.value))
   }
   return (
     <main>
       <header>
         <h1>Hello Utrust</h1>
         <form onSubmit={onSubmit}>
-          <input type='number' />
+          <input
+            type='number'
+            name='quantity'
+            step={1}
+            min={minQuantity}
+            value={quantity}
+            onChange={onQuantityChange}
+          />
           <input type='submit' />
         </form>
       </header>
