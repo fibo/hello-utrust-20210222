aa424f0afd1ba66f8d6ff39b413e80f70280d080
Matteo Dal Zovo <matteo.dalzovo@zextras.com>
2021-02-22 17:07:47 +0100

    Memoize callbacks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 const minQuantity = 1
 
@@ -10,41 +10,50 @@ function CoerceQuantity(value) {
 
 export function App() {
   const [quantity, setQuantity] = useState(minQuantity)
-  const onSubmit = (event) => {
+  const [submitting, setSubmitting] = useState(false)
+
+  const onSubmit = useCallback((event) => {
     event.preventDefault()
     console.log('on submit')
-  }
-  const onQuantityChange = (event) => {
+    setSubmitting(true)
+  }, [])
+
+  const onQuantityChange = useCallback((event) => {
     setQuantity(CoerceQuantity(event.target.value))
-  }
+  }, [])
+
+  const onKeyDown = useCallback((event) => {
+    const isPeriodOrComma = ['Comma', 'Period', 'NumpadDecimal'].includes(
+      event.code
+    )
+    if (isPeriodOrComma) {
+      event.preventDefault()
+    }
+  }, [])
+
+  const onChange = useCallback(
+    (event) => {
+      setQuantity(event.target.value)
+    },
+    [setQuantity]
+  )
+
   return (
     <main>
       <header>
         <h1>Hello Utrust</h1>
         <form onSubmit={onSubmit}>
           <input
-            onKeyDown={(event) => {
-              const isPeriodOrComma = [
-                'Comma',
-                'Period',
-                'NumpadDecimal'
-              ].includes(event.code)
-
-              if (isPeriodOrComma) {
-                event.preventDefault()
-              }
-            }}
+            onKeyDown={onKeyDown}
             type='number'
             name='quantity'
             step={1}
             min={minQuantity}
             value={quantity}
             onBlur={onQuantityChange}
-            onChange={(event) => {
-              setQuantity(event.target.value)
-            }}
+            onChange={onChange}
           />
-          <input type='submit' />
+          <input type='submit' disabled={submitting} />
         </form>
       </header>
     </main>
