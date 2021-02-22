3cc4fa123f98025c937a69b26011d847b229f172
Lisa Parma <lisa.parma@zextras.com>
2021-02-22 14:52:45 +0100

    Add form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 export function App() {
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log('on submit')
+  }
   return (
     <main>
       <header>
         <h1>Hello Utrust</h1>
+        <form onSubmit={onSubmit}>
+          <input type='number' />
+          <input type='submit' />
+        </form>
       </header>
     </main>
   )
